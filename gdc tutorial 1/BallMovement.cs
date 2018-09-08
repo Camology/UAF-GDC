95c4ce8fbfa7078bc31f91542b3132945ca73926
Cameron K. Titus <wirelessatari@gmail.com>
2018-09-08 14:54:35 -0800

    added some scripts

---
+++ gdc tutorial 1/BallMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+	public float speed = 50;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2d(Collision2D col) {
+		if (col.gameObject.name == "RacketLeft" || col.gameObject.name == "RacketRight") {
+			Vector2 dir = new Vector2 ();
+			float y = hitFactor (transform.position,
+				          		col.transform.position,
+				          		col.collider.bounds.size.y);
+			if (col.gameObject.name == "RacketLeft") {
+				dir = new Vector2 (1, y).normalized;
+			} else {
+				dir = new Vector2 (-1, y).normalized;
+			}
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+		}
+
+		if (col.gameObject.name == "WallLeft" || col.gameObject.name == "WallRight") {
+			//destroyBall ();
+		}
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+}
