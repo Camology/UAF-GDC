15560c3fffba4a43aec09a586c6685187d0c2b60
Cameron K. Titus <wirelessatari@gmail.com>
2018-09-22 22:15:52 -0800

    files for flappy bird stuff

---
+++ gdc tutorial 3/CameraMovin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovin : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (target.position.x,
+										  transform.position.y, 
+										  transform.position.z);
+	}
+
+
+}
